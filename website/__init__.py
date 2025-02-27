15de461ad2f854784607ba078568d8432e0c6be7
parulhardaha <phardaha5@gmail.com>
2025-02-27 13:44:38 +0530

    Fixed Home Page Stuffs

--- website/__init__.py
+++ website/__init__.py
@@ -13,7 +13,10 @@ def create_database():
 
 
 def create_app():
-    app= Flask(__name__)
+    from flask import Flask
+
+    app = Flask(__name__)
+
     app.config['SECRET_KEY']="parulhardaha"
     app.config['SQLALCHEMY_DATABASE_URI']=f'sqlite:///{DB_NAME}'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  #Added this line to ignore
